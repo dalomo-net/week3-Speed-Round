9f8bda796bb0080ca877662cf92e9fbd900de876
PIERCE <etreraviauseptiemeciel@gmail.com>
2021-03-17 01:03:11 +0900

    give up

--- app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/MainActivity.kt
@@ -19,14 +19,22 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFromBaseline
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material.BottomAppBar
 import androidx.compose.material.Button
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Scaffold
@@ -37,6 +45,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.SpanStyle
@@ -82,7 +93,7 @@ fun MyApp() {
                 Login(navController = navController)
             }
             composable("home") {
-                Home(navController = navController)
+                Home()
             }
         }
     }
@@ -94,15 +105,12 @@ fun Welcome(navController: NavController) {
         backgroundColor = MaterialTheme.colors.primary
     ) {
 
-        Box(
-
-        ) {
+        Box() {
             Image(
                 painter = painterResource(id = R.drawable.light_welcome_bg),
                 contentDescription = ""
             )
             Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                // Spacer(modifier = Modifier.height(72.dp))
 
                 Image(
                     modifier = Modifier
@@ -124,7 +132,8 @@ fun Welcome(navController: NavController) {
                 Button(
                     onClick = { },
                     shape = MaterialTheme.shapes.medium,
-                    modifier = Modifier.fillMaxWidth()
+                    modifier = Modifier
+                        .fillMaxWidth()
                         .height(48.dp)
                         .padding(top = 40.dp)
 
@@ -138,7 +147,8 @@ fun Welcome(navController: NavController) {
                 Button(
                     onClick = { navController.navigate("login") },
                     shape = MaterialTheme.shapes.medium,
-                    modifier = Modifier.fillMaxWidth()
+                    modifier = Modifier
+                        .fillMaxWidth()
                         .height(48.dp)
                         .padding(top = 8.dp)
                 ) {
@@ -148,10 +158,8 @@ fun Welcome(navController: NavController) {
                         style = MaterialTheme.typography.button
                     )
                 }
-
             }
         }
-
     }
 }
 
@@ -164,7 +172,7 @@ fun Login(navController: NavController) {
     Scaffold() {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.padding(16.dp)
+            modifier = Modifier.padding(16.dp)
         ) {
             Text(
                 text = "Log in with email",
@@ -208,29 +216,199 @@ fun Login(navController: NavController) {
                 },
                 style = MaterialTheme.typography.body2,
                 textAlign = TextAlign.Center,
-                modifier = Modifier.padding(top = 24.dp,)
+                modifier = Modifier.padding(top = 24.dp)
             )
             Button(
                 onClick = { navController.navigate("home") },
                 shape = MaterialTheme.shapes.medium,
-            modifier = Modifier.fillMaxWidth()
-                .height(48.dp)
-                .padding(top = 16.dp)
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(48.dp)
+                    .padding(top = 16.dp)
             ) {
                 Text(
                     text = "Log in",
                     color = MaterialTheme.colors.secondary,
                     style = MaterialTheme.typography.button
                 )
-
             }
-
         }
     }
 }
 
 @Composable
-fun Home(navController: NavController) {
+fun Home() {
+
+    Scaffold(bottomBar = { BottomBar() }) {
+        Column(modifier = Modifier.padding(16.dp)) {
+
+            Spacer(modifier = Modifier.height(40.dp))
+
+            OutlinedTextField(
+                value = "Search", onValueChange = { },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(56.dp)
+            )
+
+            Text(
+                text = "Browse themes",
+                style = MaterialTheme.typography.h1,
+                color = MaterialTheme.colors.onPrimary,
+                modifier = Modifier.paddingFromBaseline(top = 32.dp)
+            )
+
+            LazyRowList()
+            Text(
+                text = "Design your home garden",
+                style = MaterialTheme.typography.h1,
+                color = MaterialTheme.colors.onPrimary
+            )
+
+            LazyColumnList()
+        }
+    }
+}
+
+@Composable
+fun BottomBar() {
+
+    BottomAppBar(
+        backgroundColor = MaterialTheme.colors.primary,
+        modifier = Modifier.height(56.dp)
+    ) {
+        Row(
+            modifier = Modifier.padding(16.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+
+            Column() {
+                Image(
+                    painter = painterResource(id = R.drawable.home),
+                    contentDescription = "Home",
+                    colorFilter = ColorFilter.tint(Color.Black)
+                )
+                Text("Home", style = MaterialTheme.typography.caption)
+            }
+            Column() {
+                Image(
+                    painter = painterResource(id = R.drawable.favarites),
+                    contentDescription = "",
+                    colorFilter = ColorFilter.tint(Color.LightGray)
+                )
+                Text("Favorites", style = MaterialTheme.typography.caption)
+            }
+
+            Column() {
+                Image(
+                    painter = painterResource(id = R.drawable.profile),
+                    contentDescription = "Home",
+                    colorFilter = ColorFilter.tint(Color.LightGray)
+                )
+                Text("Profile", style = MaterialTheme.typography.caption)
+            }
+            Column() {
+                Image(
+                    painter = painterResource(id = R.drawable.cart),
+                    contentDescription = "Home",
+                    colorFilter = ColorFilter.tint(Color.LightGray)
+                )
+                Text("Cart", style = MaterialTheme.typography.caption)
+            }
+        }
+    }
+}
+
+@Composable
+fun LazyColumnList() {
+    LazyColumn(
+        content = {
+            item {
+                KindCard(n = "Monstera", rid = R.drawable.monstera)
+            }
+            // item {
+            //     KindCard(n = "Aglaonema", rid = R.drawable.aglaonema)
+            // }
+            // item {
+            //     KindCard(n = "Peace Lily", rid = R.drawable.peace_lilly)
+            // }
+            // item {
+            //     KindCard(n = "Fiddle Leaf tree", rid = R.drawable.fiddle_leaf)
+            // }
+            // item {
+            //     KindCard(n = "Snake plant", rid = R.drawable.snake_plant)
+            // }
+            // item {
+            //     KindCard(n = "Pothos", rid = R.drawable.pothos)
+            // }
+        }
+    )
+}
+
+@Composable
+fun KindCard(n: String, rid: Int) {
+    Row(
+        modifier = Modifier.height(64.dp)
+    ) {
+        Image(
+            painter = painterResource(id = rid),
+            contentDescription = "",
+            modifier = Modifier.clip(shape = MaterialTheme.shapes.small)
+        )
+        Column(modifier = Modifier.padding(start = 16.dp)) {
+            Text(
+                text = n,
+                style = MaterialTheme.typography.h2,
+                color = MaterialTheme.colors.onPrimary
+            )
+            Text(
+                text = "This is a description",
+                style = MaterialTheme.typography.body1,
+                color = MaterialTheme.colors.onPrimary
+            )
+            Divider()
+        }
+    }
+}
+
+@Composable
+fun LazyRowList() {
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        content = {
+            item { ThemeCard("Desert chic", R.drawable.desert_chic) }
+            // item { ThemeCard("Tiny terrariums", R.drawable.tiny_terrariums) }
+            // item { ThemeCard("Jungle vibes", R.drawable.jungle_vibes) }
+            // item { ThemeCard("Easy care", R.drawable.easy_care) }
+            // item { ThemeCard("Statements", R.drawable.statements) }
+        }
+    )
+}
+
+@Composable
+fun ThemeCard(n: String, rid: Int) {
+
+    Box(
+        modifier = Modifier
+            .height(136.dp)
+            .width(136.dp)
+    ) {
+        Image(
+            painter = painterResource(id = rid),
+            contentDescription = "",
+            modifier = Modifier.clip(shape = MaterialTheme.shapes.small),
+            contentScale = ContentScale.Fit
+        )
+        Text(
+            text = n,
+            modifier = Modifier
+                .wrapContentSize()
+                .padding(start = 16.dp),
+            textAlign = TextAlign.Left,
+            style = MaterialTheme.typography.h2,
+            color = MaterialTheme.colors.onPrimary
+        )
+    }
 }
 
 @Preview("Light Theme", widthDp = 360, heightDp = 640)
